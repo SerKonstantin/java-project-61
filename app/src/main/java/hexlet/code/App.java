cf56c0fcbb7bc08da314b8576e4f1dbec1a35ea2
serebrianskii <serebrianskiik@gmail.com>
2023-07-23 17:13:38 +0300

    Add prototype for choosing a game mode

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,45 @@
 package hexlet.code;
 
+import java.util.Scanner;
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        // Game modes
+        // For appending new game mode one needs to append this HashMap and switch below
+        HashMap<String, String> gameModes = new HashMap<>();
+        gameModes.put("0", "Exit");
+        gameModes.put("1", "Greet");
 
-        String userName = Cli.getName();
+        System.out.println("Please enter the game number and press Enter.");
+        for (String key : gameModes.keySet()) {
+            System.out.println(key + " - " + gameModes.get(key));
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        String gameModeChoice = scanner.next();
+        if (gameModes.get(gameModeChoice) == null) {
+            System.out.println("Error: Not a valid game number");
+            return;
+        } else {
+            System.out.println("Your choice: " + gameModeChoice);
+        }
+
+        // For appending new game mode one needs to append this switch and HashMap "gameModes"
+        switch (gameModes.get(gameModeChoice)) {
+            case "Exit":
+                System.out.println("Exiting game now. Hope you had fun)");
+                return;
+
+            case "Greet":
+                System.out.println("Welcome to the Brain Games!");
+                String userName = Cli.getName();
+                break;
+
+            // Default case only needed if somebody add new game mode to the HashMap and doesn't add it to the switch
+            default:
+                System.out.println("Error: Sorry, game isn't implemented yet");
+                return;
+        }
     }
 }
