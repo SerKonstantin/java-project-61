63e3c6a2490e1f8e533464991013e2e1fdf95b19
serebrianskii <serebrianskiik@gmail.com>
2023-08-04 16:27:43 +0300

    Refactor Engine class

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -6,50 +6,30 @@ public class Engine {
     public static final int ROUNDS_COUNT = 3;
 
     public static void startGame(String rule, String[] questions, String[] correctAnswers) {
-        String userName = getName();
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
 
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
         System.out.println(rule);
 
         for (int i = 0; i < ROUNDS_COUNT; i++) {
             System.out.println(questions[i]);
-            String answer = normalizeAnswer(getAnswer());
+            System.out.print("Your answer: ");
+            String answer = normalizeAnswer(scanner.next());
 
-            var isCorrect = checkAnswer(answer, correctAnswers[i], userName);
-            if (!isCorrect) {
+            if (answer.equals(correctAnswers[i])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswers[i]
+                                   + "'.");
+                System.out.println("Let's try again, " + userName + "!");
                 return;
             }
         }
 
         System.out.println("Congratulations, " + userName + "!");
-    }
-
-
-    public static String getName() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        Scanner scanner = new Scanner(System.in);
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-        return userName;
-    }
-
-
-    public static String getAnswer() {
-        System.out.print("Your answer: ");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.next();
-    }
-
-
-    public static boolean checkAnswer(String answer, String correctAnswer, String userName) {
-        if (answer.equals(correctAnswer)) {
-            System.out.println("Correct!");
-            return true;
-        } else {
-            System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
-            System.out.println("Let's try again, " + userName + "!");
-            return false;
-        }
     }
 
 
